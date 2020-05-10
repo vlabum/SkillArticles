43bc23d97bcaa145632dd598e2573248cfeaa577
vlabu <vlabum@gmail.com>
2020-05-10 09:36:52 +0500

    hometask_7_7

--- app/src/main/java/ru/skillbranch/skillarticles/ui/article/ArticleFragment.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/article/ArticleFragment.kt
@@ -254,10 +254,10 @@ class ArticleFragment : BaseFragment<ArticleViewModel>(), IArticleView {
             }
         }
 
-        private var searchResults: List<Pair<Int, Int>> by RenderProp(emptyList())
+        private var searchResults: List<Pair<Int, Int>> by RenderProp<List<Pair<Int, Int>>>(emptyList())
         private var searchPosition: Int by RenderProp(0)
 
-        private var content: List<MarkdownElement> by RenderProp(emptyList()) {
+        private var content: List<MarkdownElement> by RenderProp<List<MarkdownElement>>(emptyList()) {
             // tv_text_content.isLoading = it.isEmpty()
             tv_text_content.setContent(it)
             if (it.isNotEmpty()) setupCopyListener()
